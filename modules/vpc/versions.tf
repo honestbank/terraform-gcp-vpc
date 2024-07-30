aa11b9c1a0b6110986e537040b2e8b0099603c32
Bibek Rauniyar <bibek@honestbank.com>
2024-07-30 13:19:18 +0700

    fix: code cleanup

--- modules/vpc/versions.tf
+++ modules/vpc/versions.tf
@@ -4,12 +4,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 5.0"
+      version = ">= 4.0, < 6.0"
     }
   }
 }
