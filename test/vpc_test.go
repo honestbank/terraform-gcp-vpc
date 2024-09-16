6474495d8549cb8b13e5c173b1af6259b5b6ddb8
Christian Witts <cwitts@gmail.com>
2024-09-16 14:24:27 +0700

    feat: Update Go dependencies for test cases [DEVOP-4980] (#24)
    * chore(deps): Update Go dependencies for test cases
    
    Refs: #DEVOP-4980
    
    Signed-off-by: Christian Witts <christian@honestbank.com>
    
    * feat: Modify the test case to use terratest/modules/terraform DEVOP-4980
    
    ---------
    
    Signed-off-by: Christian Witts <christian@honestbank.com>
    Co-authored-by: Sagar Trivedi <sagar@honestbank.com>

--- test/vpc_test.go
+++ test/vpc_test.go
@@ -2,119 +2,85 @@ package test
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 	"strings"
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/files"
-	"github.com/gruntwork-io/terratest/modules/gcp"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
-
-	"github.com/gruntwork-io/terratest/modules/logger"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestVpc(t *testing.T) {
 	runId := strings.ToLower(random.UniqueId())
 	name := fmt.Sprintf("terratest-%s", runId)
 
-	gcpCredentials := gcp.GetGoogleCredentialsFromEnvVar(t)
 	primarySubnetName := "primary-" + runId
 	podsIpRangeName := "pods-" + runId
 	servicesIpRangeName := "services-" + runId
-	tempTestDir := ""
-	vpcModulePathKey := "vpcModulePath"
-
-	// Files to copy into test folder
-	varFile := "wrapper.auto.tfvars"
-	providerFile := "providers.tf"
 
 	t.Run(name, func(t *testing.T) {
 
 		t.Parallel()
-		workingDir := filepath.Join(".", "test-runs", name)
+		testDirectory := test_structure.CopyTerraformFolderToTemp(t, "../modules/vpc", ".")
 
-		test_structure.RunTestStage(t, "create_test_copy", func() {
-			tempTestDir = test_structure.CopyTerraformFolderToTemp(t, "../modules/vpc", ".")
-			logger.Logf(t, "path to test folder %s\n", tempTestDir)
-
-			test_structure.SaveString(t, workingDir, vpcModulePathKey, tempTestDir)
+		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+			TerraformDir: testDirectory,
+			Vars: map[string]interface{}{
+				"network_name":                         name,
+				"vpc_primary_subnet_name":              primarySubnetName,
+				"vpc_secondary_ip_range_pods_name":     podsIpRangeName,
+				"vpc_secondary_ip_range_services_name": servicesIpRangeName,
+				"google_project":                       "tf-shared-vpc-host-78a3",
+				"google_region":                        "asia-southeast2",
+				"vpc_routing_mode":                     "REGIONAL",
+				"vpc_primary_subnet_ip_range_cidr":     "10.10.0.0/16",
+				"vpc_secondary_ip_range_pods_cidr":     "10.20.0.0/16",
+				"vpc_secondary_ip_range_services_cidr": "10.30.0.0/16",
+			},
 		})
 
-		test_structure.RunTestStage(t, "create_terratest_options", func() {
+		// Destroying terraform resources with 3 retries
+		defer func() {
+			terraform.DestroyE(t, terraformOptions)
+		}()
 
-			vpcModulePath := test_structure.LoadString(t, workingDir, vpcModulePathKey)
-			//terraformVars := map[string]interface{}{
-			//	"google_region":      gcpIndonesiaRegion,
-			//	"google_project":     gcpProject,
-			//	"google_credentials": gcpCredentials,
-			//	"stage":              "test",
-			//}
+		// Apply the Terraform configuration
+		_, err := terraform.InitAndApplyE(t, terraformOptions)
+		assert.NoError(t, err, "Terraform apply failed")
 
-			vpcTerratestOptions := &terraform.Options{
-				TerraformDir: vpcModulePath,
-				VarFiles:     []string{varFile},
-				Vars: map[string]interface{}{
-					"google_credentials":                   gcpCredentials,
-					"network_name":                         name,
-					"vpc_primary_subnet_name":              primarySubnetName,
-					"vpc_secondary_ip_range_pods_name":     podsIpRangeName,
-					"vpc_secondary_ip_range_services_name": servicesIpRangeName,
-				},
-			}
+		// Validate outputs
+		networkName := terraform.Output(t, terraformOptions, "network_name")
+		assert.Equal(t, name, networkName, "Network name mismatch")
 
-			logger.Log(t, "Created vpcTerratestOptions: ", vpcTerratestOptions)
-			test_structure.SaveTerraformOptions(t, workingDir, vpcTerratestOptions)
-		})
+		networkId := terraform.Output(t, terraformOptions, "network_id")
+		assert.NotEmpty(t, networkId, "Network ID should not be empty")
 
-		testFileSourceDir, getTestDirErr := os.Getwd()
-		if getTestDirErr != nil {
-			fmt.Println("calling t.FailNow(): could not execute os.Getwd(): ", getTestDirErr)
-			t.FailNow()
-		}
+		primarySubnetNameOutput := terraform.Output(t, terraformOptions, "primary_subnet_name")
+		assert.Equal(t, primarySubnetName, primarySubnetNameOutput, "Primary Subnet name mismatch")
 
-		fmt.Println("test working directory is: ", testFileSourceDir)
+		primarySubnetIpCidrRange := terraform.Output(t, terraformOptions, "primary_subnet_ip_cidr_range")
+		assert.Equal(t, "10.10.0.0/16", primarySubnetIpCidrRange, "Primary Subnet CIDR range mismatch")
 
-		filesToCopy := []string{varFile, providerFile}
+		podsSubnetName := terraform.Output(t, terraformOptions, "pods_subnet_name")
+		assert.Equal(t, podsIpRangeName, podsSubnetName, "Pods Subnet name mismatch")
 
-		fmt.Println("copying files: ", filesToCopy, " to temporary test dir: ", tempTestDir)
-		for _, file := range filesToCopy {
-			src := testFileSourceDir + "/" + file
-			dest := tempTestDir + "/" + file
-			copyErr := files.CopyFile(src, dest)
-			if copyErr != nil {
-				fmt.Println("😩 calling t.FailNow(): failed copying from: ", src, " to: ", dest, " with error: ", copyErr)
-				t.FailNow()
-			} else {
-				fmt.Println("✌️ Success! Copied from: ", src, " to: ", dest)
-			}
-		}
+		podsSubnetCidr := terraform.Output(t, terraformOptions, "pods_subnet_cidr")
+		assert.Equal(t, "10.20.0.0/16", podsSubnetCidr, "Pods Subnet CIDR range mismatch")
 
-		defer test_structure.RunTestStage(t, "cleanup/destroy", func() {
-			vpcTerratestOptions := test_structure.LoadTerraformOptions(t, workingDir)
-			_, destroyErr := terraform.DestroyE(t, vpcTerratestOptions)
-			assert.Nil(t, destroyErr, "terraform destroy errored: ", destroyErr)
-		})
+		servicesSubnetName := terraform.Output(t, terraformOptions, "services_subnet_name")
+		assert.Equal(t, servicesIpRangeName, servicesSubnetName, "Services Subnet name mismatch")
 
-		test_structure.RunTestStage(t, "init", func() {
-			vpcTerratestOptions := test_structure.LoadTerraformOptions(t, workingDir)
-			_, initErr := terraform.InitE(t, vpcTerratestOptions)
-			assert.Nil(t, initErr, "terraform init errored: ", initErr)
-		})
+		servicesSubnetCidr := terraform.Output(t, terraformOptions, "services_subnet_cidr")
+		assert.Equal(t, "10.30.0.0/16", servicesSubnetCidr, "Services Subnet CIDR range mismatch")
 
-		test_structure.RunTestStage(t, "plan", func() {
-			vpcTerratestOptions := test_structure.LoadTerraformOptions(t, workingDir)
-			_, planErr := terraform.PlanE(t, vpcTerratestOptions)
-			assert.Nil(t, planErr, "terraform plan errored: ", planErr)
-		})
+		primarySubnetSelfLink := terraform.Output(t, terraformOptions, "primary_subnet_self_link")
+		assert.NotEmpty(t, primarySubnetSelfLink, "Primary Subnet Self Link should not be empty")
 
-		test_structure.RunTestStage(t, "apply", func() {
-			vpcTerratestOptions := test_structure.LoadTerraformOptions(t, workingDir)
-			_, applyErr := terraform.InitAndApplyE(t, vpcTerratestOptions)
-			assert.Nil(t, applyErr, "terraform apply errored: ", applyErr)
-		})
+		sharedVpcId := terraform.Output(t, terraformOptions, "shared_vpc_id")
+		assert.Equal(t, networkId, sharedVpcId, "Shared VPC ID mismatch")
+
+		sharedVpcSelfLink := terraform.Output(t, terraformOptions, "shared_vpc_self_link")
+		assert.NotEmpty(t, sharedVpcSelfLink, "Shared VPC Self Link should not be empty")
 	})
 }
